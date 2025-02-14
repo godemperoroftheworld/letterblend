615775c0b79c3a518cea00cca8ca22dbecb1d7bc
Tenzin Pelletier <tenzin@hivestack.com>
2025-02-14 11:53:04 -0500

    fix: revert bad change

--- frontend/src/composables/user.ts
+++ frontend/src/composables/user.ts
@@ -2,9 +2,15 @@ const useUser = createSharedComposable(() => {
   const user = useCookie<string>('user', {
     watch: true,
   });
+  const internalUser = computed({
+    get: () => user.value ?? '',
+    set(val) {
+      user.value = val;
+    },
+  });
   const isSet = computed(() => !!user.value?.length);
   const refresh = () => refreshCookie('user');
-  return { user, isSet, refresh };
+  return { user: internalUser, isSet, refresh };
 });
 
 export default useUser;
