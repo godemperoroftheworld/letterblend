b1e54613d6dc1b7f7d66ba49becbfc3d88fdf012
Tenzin Pelletier <tenzin@perion.com>
2025-02-25 22:31:29 -0500

    fix: only do auth on client

--- frontend/src/middleware/user.global.ts
+++ frontend/src/middleware/user.global.ts
@@ -2,8 +2,10 @@ import useUser from '@/composables/user';
 
 export default defineNuxtRouteMiddleware((to) => {
   const { exists } = useUser();
+  const router = useRouter();
   const excludePaths = ['/', '/auth'];
+  if (import.meta.server) return;
   if (!excludePaths.includes(to.path) && !exists.value) {
-    return navigateTo(`/auth?redirect=${to.path}`);
+    router.replace(`/auth?redirect=${to.path}`);
   }
 });
